52ab5027ecc5de90bd9b7757d426e39c6cfca2dc
Harold Sanchez <sanchezharold13@gmail.com>
2022-06-23 11:38:11 -0500

    Nuevos cambios

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-//Variables creadas
-let name = 'John';
-let admin = name;
+// Se agrega este nombre porque es el planeta donde habitamos
+const nuestroPlaneta = 'Tierra';
+// Se agrega el nombre del usuario actual de la pag
+let currentUserName = 'carlos';
 
-//Imprimiendo el nombre
-console.log(admin);
-console.log(name);
+//impresion de resultados
+console.log(nuestroPlaneta);
+console.log(currentUserName);
