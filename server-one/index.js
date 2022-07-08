ea1b91d77340c6e0219cce4cfb6816c01a0150ee
Harold Sanchez <sanchezharold13@gmail.com>
2022-07-08 15:26:58 -0500

    aprendiendo mas node

---
+++ server-one/index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const app = http.createServer((request, respose) => {
+    respose.writeHead(200, { 'Content-type': 'text/plain' });
+    respose.end('Hello world');
+})
+
+const PORT = 3000;
+
+app.listen(PORT);
+
+console.log(`Server runnin on port ${PORT}`)
